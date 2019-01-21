f5f7a8c6fad244e7dca64a9a4e240902104e448f
rayweb <raymundo.armendariz@gmail.com>
2019-01-21 14:50:10 -0700

    binary count

---
+++ src/main/java/snippets/binary.java
@@ -0,0 +1,13 @@
+package snippets;
+public final class binary {
+    public static void main(String[] args) {
+        int bits = countBits(1234);
+        System.out.println(bits);
+    }
+    public static int countBits(int n){
+        String binaryRepresentation = Integer.toString(n,2);
+        System.out.println(binaryRepresentation);
+        long count = binaryRepresentation.chars().filter(ch -> ch == '1').count();
+        return (int)count;
+	}
+}
